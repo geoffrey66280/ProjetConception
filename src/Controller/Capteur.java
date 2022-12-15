82468a6cf85fd100c9b5505b0422d78db547d363
LouisLangellier <80830821+LouisLangellier@users.noreply.github.com>
2022-12-15 15:12:46 +0100

    Capteurs et salle v2

--- src/Controller/Capteur.java
+++ src/Controller/Capteur.java
@@ -1,30 +1,49 @@
 package Controller;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class Capteur {
 
     private String reference;
     private Centrale centrale;
+    private Salle salle;
     private float temp;
     private float elec;
     private int min = 5;
     private int max = 19;
 
-    public Capteur(String reference){
+    public Capteur(String reference, Salle salle){
         this.reference = reference;
+        this.salle = salle;
+        this.temp = 0;
     }
 
-    public void MesurerTemperature(){
+    public String getReference() {
+        return reference;
+    }
+
+    public void mesurerTemperature(){
         Random r = new Random();
         this.temp = min + r.nextFloat() * (max - min);
     }
 
-    public void TransmettreTemperature(){
-
+    public String transmettreTemperature(){
+        mesurerTemperature();
+        for(Capteur c : this.salle.getCapteurs()){
+            if(Objects.equals(c.getReference(), this.getReference())){
+                c.setTemp(this.temp);
+            }
+        }
+        this.salle.addCapteurs(this);
+        return "Capteur ajouté";
     }
 
     public float getTemp(){
         return this.temp;
+    }
+
+    public void setTemp(float temp) {
+        this.temp = temp;
     }
 }
