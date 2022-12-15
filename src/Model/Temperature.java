a590ee5650f7ace31eeed3c8d52ef10f25a00575
geoffrey66280 <geoffrey160502@gmail.com>
2022-12-15 14:19:26 +0100

    init tableaudebord et temperature

--- src/Model/Temperature.java
+++ src/Model/Temperature.java
@@ -1,4 +1,43 @@
 package Model;
 
+import java.util.Date;
+
 public class Temperature {
+
+    private int date;
+    private String refCapteur;
+    private double temperature;
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getRefCapteur() {
+        return refCapteur;
+    }
+
+    public void setRefCapteur(String refCapteur) {
+        this.refCapteur = refCapteur;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public Temperature(int date, String refCapteur, double temperature) {
+        this.date = date;
+        this.refCapteur = refCapteur;
+        this.temperature = temperature;
+    }
+
+
+
 }
