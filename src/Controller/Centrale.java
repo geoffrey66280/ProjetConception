949e11be17ae6e02136b25dd5356514fccaf3029
Jose G Perez <55620570+joseregpo@users.noreply.github.com>
2022-12-15 15:22:08 +0100

    getTemperatures

--- src/Controller/Centrale.java
+++ src/Controller/Centrale.java
@@ -2,18 +2,27 @@ package Controller;
 
 import Model.Temperature;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
 
 public class Centrale {
-    private float seuilElectricite;
+    private float seuilElectricite = 50;
+    private float electriciteActuel;
     private ArrayList<Temperature> temperatures;
     private ArrayList<Salle> salles;
     private ArrayList<Gestionnaire> gestionnaires;
 
+    public Centrale(){
+        Random r = new Random();
+        this.electriciteActuel = r.nextFloat(0,100);
+    }
+
     public void updateEtatElectricite(){
 
     }
-
 
     public void allumerChauffage(){
 
@@ -22,28 +31,24 @@ public class Centrale {
     public void couperChauffage(){
 
     }
-
-    public void stockerTemp(){
-
+    /*
+    Enregistre un objet Temperature
+     */
+    public void stockerTemp( int d, String ref,double t){
+        this.temperatures.add(new Temperature(d,ref,t));
     }
-    public void suscribe(Capteur c){
-
-    }
-
-
-    public void setGestionnaires(ArrayList<Gestionnaire> gestionnaires) {
-        this.gestionnaires = gestionnaires;
-    }
-
-    public void setSalles(ArrayList<Salle> salles) {
-        this.salles = salles;
+    /*
+        Souscrit la centrale à une salle
+     */
+    public void suscribe(Salle s){
+        this.salles.add(s);
     }
 
-    public void setSeuilElectricite(float seuilElectricite) {
-        this.seuilElectricite = seuilElectricite;
-    }
-
-    public void setTemperatures(ArrayList<Temperature> temperatures) {
-        this.temperatures = temperatures;
+    public ArrayList<Temperature> getTemperatures() {
+        ArrayList<Temperature> temps = new ArrayList<Temperature>();
+        for (int i=0; i<10; i++){
+            temps.add(temperatures.get(i));
+        }
+        return temps;
     }
 }
