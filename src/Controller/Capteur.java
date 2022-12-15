8691c65b5b9419e422c8757a0ba86d9adfc5289b
Jose G Perez <55620570+joseregpo@users.noreply.github.com>
2022-12-15 17:12:52 +0100

    Commentaires

--- src/Controller/Capteur.java
+++ src/Controller/Capteur.java
@@ -8,22 +8,39 @@ public class Capteur implements CapteurObserver{
     private Centrale centrale;
     private double temp;
 
+    /**
+     * Instancie un Capteur
+     * @param reference
+     * @param centrale
+     */
     public Capteur(String reference, Centrale centrale){
         this.reference = reference;
         this.temp = 0;
         this.centrale = centrale;
     }
 
+    /**
+     * Renvoie la reference du Capteur
+     * @return
+     */
     public String getReference() {
         return reference;
     }
 
+    /**
+     * Mesure la temperature de la salle où le capteur est
+     *
+     * Nous utilisons un random pour faire la simulation
+     */
     public void mesurerTemperature(){
         Random r = new Random();
         this.temp = 5 + r.nextFloat() * (19 - 5);
         notifyCentral();
     }
 
+    /**
+     * Redefinition de notify de l'interface Gestionnaire
+     */
     @Override
     public void notifyCentral() {
         this.centrale.verifierTemperature(this, this.temp);
--- src/Controller/Centrale.java
+++ src/Controller/Centrale.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Centrale {
+
     private double seuilElectricite = 20;
     private double seuilTemp = 10;
     private double electriciteActuel;
     private ArrayList<Temperature> temperatures;
     private ArrayList<Gestionnaire> gestionnaires;
 
+    /**
+     * Constructeur
+     */
     public Centrale(){
         Random r = new Random();
         this.gestionnaires = new ArrayList<>();
@@ -18,12 +22,21 @@ public class Centrale {
         this.electriciteActuel = r.nextFloat(0,100);
     }
 
+    /**
+     * Notifie tous les gestionnaires souscrits à la centrale
+     * @param bool
+     */
     public void notifyGestionnaire(boolean bool){
         for (Gestionnaire gest : this.gestionnaires){
             gest.update(bool);
         }
     }
 
+    /**
+     * Verifie la temperature envoyée par le capteur lors d'une notification
+     * @param c
+     * @param temp
+     */
     public void verifierTemperature(Capteur c, double temp){
         stockerTemp(0102, c.getReference(), temp);
         System.out.println("Electricite actuelle " + this.electriciteActuel);
@@ -35,6 +48,13 @@ public class Centrale {
             notifyGestionnaire(false);
         }
     }
+
+    /**
+     * Verifie la consomation d'electricité actuelle
+     *
+     *  Pour l'instant nous utilisons un random pour faire une simulation
+     * @return
+     */
     public boolean verifierElectricite(){
         Random r = new Random();
         this.electriciteActuel = r.nextFloat(0,100);
@@ -45,23 +65,31 @@ public class Centrale {
         }
     }
 
-    /*
-    Enregistre un objet Temperature
+    /**
+     * Enregistre la temperature dans le tableau de temperatures
+     * @param d
+     * @param ref
+     * @param t
      */
     public void stockerTemp(int d, String ref, double t){
         this.temperatures.add(new Temperature(d,ref,t));
     }
 
-    /*
-    Ajoute un gestionnaire aux souscripteur
+    /**
+     * Ajoute un Gestionnaire d'electricité pour le notifier des changements
+     * @param g
      */
     public void suscribeGestionnaire(Gestionnaire g){
         this.gestionnaires.add(g);
     }
 
+    /**
+     * Renvoie les 10 premières occurrences du tableau des temperatures
+     * @return
+     */
     public ArrayList<Temperature> getTemperatures() {
         ArrayList<Temperature> temps = new ArrayList<>();
-        for (int i=0; i<temperatures.size(); i++){
+        for (int i=0; i<temperatures.size() && i<10; i++){
             temps.add(temperatures.get(i));
         }
         return temps;
